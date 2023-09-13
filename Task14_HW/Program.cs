03f8895f2f774500d827fb4c627bb682f07f3c39
«Petrov_Ruslan» <tralilivali@bk.ru>
2023-09-13 13:22:54 +0200

    add_Task14_HW

---
+++ Task14_HW/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на
+// вход трехзначное число и на выходе показывает вторую
+// цифру этого числа. Не использовать строки для расчета
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int ReadInt(string message)
+{
+    System.Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+bool Validate3sign(int number)
+{
+    if (number >= 100 && number < 1000)
+    {
+        return true;
+    }
+
+    System.Console.WriteLine("Число не трехзначное");
+    return false;
+}
+
+int num = ReadInt("Введите трехзначное число -> ");
+if (Validate3sign(num))
+{
+int value = num / 10 % 10;
+System.Console.WriteLine($"{value}");
+}
