78843be6ccf7d9a0b4561c9f42f6b8165bdd5093
«Petrov_Ruslan» <tralilivali@bk.ru>
2023-09-13 13:30:47 +0200

    add_Task7_HW_corrected

--- Task7_HW/Program.cs
+++ Task7_HW/Program.cs
@@ -1,4 +1,10 @@
-﻿Console.Write("Введите число => "); 
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Write("Введите число => "); 
 string inputedString1 = Console.ReadLine();
 int number1 = Convert.ToInt32(inputedString1);   
 int number2 = 2;
